3343337063282547a3abc30dc98994d2067165ed
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-17 11:47:43 +0900

    .

---
+++ src/main/java/org/koreait/member/filter/LoginFilter.java
@@ -0,0 +1,17 @@
+package org.koreait.member.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class LoginFilter extends GenericFilterBean {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        chain.doFilter(request, response);
+    }
+}
