814ea56413f271330b73d9ebe7b5108e6e5e4cbc
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-17 13:21:01 +0900

    .

--- src/main/java/org/koreait/global/configs/SecurityConfig.java
+++ src/main/java/org/koreait/global/configs/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.koreait.global.configs;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.koreait.member.filter.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -21,6 +22,7 @@ import org.springframework.web.filter.CorsFilter;
 public class SecurityConfig {
 
     private final CorsFilter corsFilter;
+    private final LoginFilter loginFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -28,7 +30,7 @@ public class SecurityConfig {
         /*
          SessionCreationPolicy
             - ALWAYS : 서버가 시작되었을때 부터 세션 생성, 세션 아이디
-            - IF_REQUIRED : 세션이 필요한 시점에 세션 을 생성(기본값)
+            - IF_REQUIRED : 세션이 필요한 시점에 세션을 생성(기본값)
             - NEVER : 세션 생성 X, 기존에 세션이 존재하면 그거는 사용
             - STATELESS : 세션 생성 X, 기본 생성된 세션도 사용 X
          */
@@ -36,6 +38,7 @@ public class SecurityConfig {
         http.csrf(c -> c.disable())
                 .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(c -> {
                     c.authenticationEntryPoint((req, res, e) -> {
                         res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
--- src/main/java/org/koreait/member/filter/LoginFilter.java
+++ src/main/java/org/koreait/member/filter/LoginFilter.java
@@ -7,7 +7,10 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.koreait.global.libs.Utils;
+import org.koreait.global.rests.JSONData;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -37,8 +40,25 @@ public class LoginFilter extends GenericFilterBean {
      * @param token
      */
     private void loginProcess(String token) {
+        if (!StringUtils.hasText(token)) {
+            return; // 토큰이 없는 일반 요청인 경우는 처리 X}
+        }
+
+        /**
+         * 1. 토큰이 있으면, member-service 인스턴스에서 회원 정보 조회
+         * 2. 로그인 처리
+         */
         try {
-            String apiUrl = utils.serviceUrl("member-service", "/info");
+            String apiUrl = utils.serviceUrl("member-service", "/");
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(token);
+
+            HttpEntity<Void> request = new HttpEntity<>(headers);
+
+            ResponseEntity<JSONData> response = restTemplate.exchange(apiUrl, HttpMethod.GET, request, JSONData.class);
+            System.out.println("-----------------------------");
+            System.out.println(response);
 
         } catch (Exception e) {
             e.printStackTrace();
